33f5115b90643a6842bb5bb9374b2e6dc10a9d69
gerudaeta <gerudaeta@gmail.com>
2022-06-19 01:56:37 -0300

    Initial Commit

---
+++ Disenio/DisenioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisenioApp.swift
+//  Disenio
+//
+//  Created by German Udaeta on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DisenioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
